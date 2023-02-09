602ad9a0ddd5a9c69720425e5f453d7bc308376d
Mauricio Gomes Santos <mau.gsantos1@gmail.com>
2023-02-09 11:45:31 -0300

    old commits

---
+++ dev/Módulo 7/main.js
@@ -0,0 +1,10 @@
+function greaterNum(){
+    var value1;
+    var value2;
+    value1 = document.getElementById("numero-conta").value;
+    value2 = document.getElementById("valor-deposito").value;
+    if (value1 > value2){
+        alert("testeeee");
+    }
+
+}
